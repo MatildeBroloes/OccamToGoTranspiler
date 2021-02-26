deb338b15d1ce9f18a484860eb4d9b76f4225991
Matilde Mouritsen Broløs <mmb@zangenberg.biz>
2021-02-26 18:32:47 +0100

    Benchmarking

--- benchmarking/bench_go.py
+++ benchmarking/bench_go.py
@@ -1,24 +1,33 @@
 import time
 import os
 
+for x in range(0,10):
+  os.system('../go_programs/count')
+
 mean1 = 0
 for x in range(0, 100):
   start = time.time()
-  os.system('echo "40\n" | go run ../go_programs/simple.go')
+  os.system('../go_programs/count')
   end = time.time()
   mean1 = mean1 + (end - start)
+
+for x in range(0,10):
+  os.system('echo "40\n" | ../go_programs/extended')
 
 mean2 = 0
 for x in range(0, 100):
   start = time.time()
-  os.system('echo "4\n" | go run ../go_programs/fancy.go')
+  os.system('echo "40\n" | ../go_programs/extended')
   end = time.time()
   mean2 = mean2 + (end - start)
+
+for x in range(0,10):
+  os.system('echo "400\n" | ../go_programs/commstime')
 
 mean3 = 0
 for x in range(0, 100):
   start = time.time()
-  os.system('echo "400\n" | go run ../go_programs/commstime.go')
+  os.system('echo "400\n" | ../go_programs/commstime')
   end = time.time()
   mean3 = mean3 + (end - start)
 
--- benchmarking/bench_occ.py
+++ benchmarking/bench_occ.py
@@ -1,6 +1,9 @@
 import time
 import os
 
+for x in range(0,10):
+  os.system('../occam-programs/count')
+
 mean1 = 0
 for x in range(0, 100):
   start = time.time()
@@ -8,12 +11,18 @@ for x in range(0, 100):
   end = time.time()
   mean1 = mean1 + (end - start)
 
+for x in range(0,10):
+  os.system('echo "40\n" | ../occam-programs/extended')
+
 mean2 = 0
 for x in range(0, 100):
   start = time.time()
-  os.system('echo "40\n" | ../occam-programs/simple')
+  os.system('echo "40\n" | ../occam-programs/extended')
   end = time.time()
   mean2 = mean2 + (end - start)
+
+for x in range(0,10):
+  os.system('echo "400\n" | ../occam-programs/commstime')
 
 mean3 = 0
 for x in range(0, 100):
